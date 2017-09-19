db70089a24ab2e72fb8a4f7f09cff5d3161dabc8
Jefferson Ward <jefferson.ward@infor.com>
2017-09-19 10:12:23 +0100

    initial

---
+++ LetMeWork.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace LetMeWork
+{
+    partial class LetMeWork
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.killItToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startStopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "Let Me Work";
+            this.notifyIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.killItToolStripMenuItem,
+            this.startStopToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(154, 88);
+            // 
+            // killItToolStripMenuItem
+            // 
+            this.killItToolStripMenuItem.Name = "killItToolStripMenuItem";
+            this.killItToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.killItToolStripMenuItem.Text = "&Kill It";
+            this.killItToolStripMenuItem.Click += new System.EventHandler(this.killItToolStripMenuItem_Click);
+            // 
+            // startStopToolStripMenuItem
+            // 
+            this.startStopToolStripMenuItem.Name = "startStopToolStripMenuItem";
+            this.startStopToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.startStopToolStripMenuItem.Text = "StartPause";
+            this.startStopToolStripMenuItem.Click += new System.EventHandler(this.startStopToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // LetMeWork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(140, 0);
+            this.Name = "LetMeWork";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Let Me Work";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem killItToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startStopToolStripMenuItem;
+    }
+}
+
---
+++ LetMeWork.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LetMeWork
+{
+    public partial class LetMeWork : Form
+    {
+        private const string StopText = "&Stop";
+        private const string StartText = "&Start";
+
+        private Thread _thread;
+        private bool _running;
+
+        public LetMeWork()
+        {
+            InitializeComponent();
+
+            notifyIcon.Icon = SystemIcons.Shield;
+            notifyIcon.ContextMenuStrip = contextMenuStrip;
+
+            startStopToolStripMenuItem.Text = StartText;
+            _running = false;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            base.OnLoad(e);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stop();
+            Application.Exit();
+        }
+
+        private void killItToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_running)
+            {
+                _thread.Interrupt();
+            }
+            else
+            {
+                KillIt();
+            }
+        }
+
+        private void startStopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_running)
+            {
+                Stop();
+            }
+            else
+            {
+                Start();
+            }
+        }
+
+        private void Start()
+        {
+            _running = true;
+            _thread = new Thread(Run);
+            _thread.Start();
+            startStopToolStripMenuItem.Text = StopText;
+        }
+
+        private void Stop()
+        {
+            if (!_running) return;
+
+            _running = false;
+            _thread.Interrupt();
+            _thread.Abort();
+            startStopToolStripMenuItem.Text = StartText;
+        }
+
+        private void Run()
+        {
+            const int tenSeconds = 10 * 1000;
+            const int increment = tenSeconds;
+            const int oneMinute = 60 * 1000;
+            const int fiveMinutes = 5 * 60 * 1000;
+
+            var sleep = tenSeconds;
+            while (_running)
+            {
+                var justKilledIt = KillIt();
+
+                if (justKilledIt)
+                {
+                    sleep = tenSeconds;
+                }
+                else
+                {
+                    sleep = sleep + increment;
+                    if (sleep > oneMinute)
+                    {
+                        sleep = fiveMinutes;
+                    }
+                }
+
+                try
+                {
+                    Thread.Sleep(sleep);
+                }
+                catch (ThreadInterruptedException)
+                {
+                }
+            }
+        }
+
+        private bool KillIt()
+        {
+            var justKilledIt = false;
+            const string processName = "mcshield";
+            var processes = Process.GetProcessesByName(processName);
+            var messages = new List<string>();
+            foreach (var process in processes)
+            {
+                process.Kill();
+                messages.Add($"Killed process {process.Id} {DateTime.Now}");
+                justKilledIt = true;
+            }
+            if (justKilledIt)
+            {
+                notifyIcon.BalloonTipText = string.Join("\r\n", messages);
+                notifyIcon.ShowBalloonTip(1000);
+            }
+            return justKilledIt;
+        }
+    }
+}
